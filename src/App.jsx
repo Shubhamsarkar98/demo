837914004c5a0adb4fe423be5a775df5005e1879
Sourabh Choudhary <111211733+Sourabh-Choudhary7@users.noreply.github.com>
2024-05-06 14:23:35 +0530

    added explore param id individual item details page (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Items from './pages/Items';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Home />} path='/' />
+        <Route element={<Items />} path='/products/:id' />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Card = ({ data }) => {
-    return (
-        <>
-            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
-                <a href="#">
-                    <img className="rounded-t-lg" src={data?.image} alt="" />
-                </a>
-                <div className="p-5">
-                    <a href="#">
-                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            {data?.category}
-                        </h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        {data?.description}
-                    </p>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
+      <Link href="#" className='flex justify-center'>
+        <img className="rounded-t-lg h-80 w-auto" src={data?.image} alt="" />
+      </Link>
+      <div className="p-5">
+        <Link href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {data?.category}
+          </h5>
+        </Link>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {data?.description}
+        </p>
+      </div>
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><Link to={`/products/${data?.id}`}>Explore more</Link></button>
+    </div>
+  )
 }
 
 export default Card
