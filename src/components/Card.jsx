a46db2c5a730b4dcad841229a210dc04eac278e2
Shubhamsarkar98 <shubhamsarkar9435@gmail.com>
2024-05-06 15:03:58 +0530

    logic of substring

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,20 +4,22 @@ import { Link } from "react-router-dom";
 const Card = ({ data }) => {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-2">
-      <Link href="#" className='flex justify-center'>
+      <Link to="#" className='flex justify-center'>
         <img className="rounded-t-lg h-80 w-auto" src={data?.image} alt="" />
       </Link>
       <div className="p-5">
-        <Link href="#">
+        <Link to="#">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {data?.category}
           </h5>
         </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {data?.description}
+            {data?.description.length > 70 ? `${data?.description.substring(0, 70)}...` : data?.description}
         </p>
       </div>
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"><Link to={`/products/${data?.id}`}>Explore more</Link></button>
+      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+        <Link to={`/products/${data?.id}`}>Explore more</Link>
+      </button>
     </div>
   )
 }
